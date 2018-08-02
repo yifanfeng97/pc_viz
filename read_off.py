3c5e8b8acb3a91e7ca2895fa1a5912c812aeb650
yifanfeng97 <1106132246@qq.com>
2018-08-02 16:31:52 +0800

    add read off function

---
+++ read_off.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def read_off(name):
+    points = []
+    faces = []
+    with open(name, 'r') as f:
+        line = f.readline().strip()
+        if line == 'OFF':
+            num_verts, num_faces, num_edge = f.readline().split()
+            num_verts = int(num_verts)
+            num_faces = int(num_faces)
+        else:
+            num_verts, num_faces, num_edge = line[3:].split()
+            num_verts = int(num_verts)
+            num_faces = int(num_faces)
+
+        for idx in range(num_verts):
+            line = f.readline()
+            point = [float(v) for v in line.split()]
+            points.append(point)
+
+        for idx in range(num_faces):
+            line = f.readline()
+            face = [int(t_f) for t_f in line.split()]
+            faces.append(face[1:])
+    triangles = np.zeros((num_faces, 3, 3))
+    for _idx, (a, b, c) in enumerate(faces):
+        triangles[_idx, 0] = np.array(points[a])
+        triangles[_idx, 1] = np.array(points[b])
+        triangles[_idx, 2] = np.array(points[c])
+
+    return triangles
+
+if __name__ == '__main__':
+    a = read_off('/home/fyf/data/std_ModelNet40/airplane/test/airplane_0627.off')
+    print(a.shape)
